d35da6f36c50d3b102b28cef058f0709289a8a49
Muhammad Syifa <emsifa@gmail.com>
2026-09-02 09:52:51 +0700

    feat: initialize Astro web project with basic structure and favicon
    - Added package.json for project configuration and dependencies.
    - Included favicon.ico and favicon.svg for branding.
    - Created index.astro as the main entry point with basic HTML structure.
    - Configured TypeScript with tsconfig.json extending Astro's strict settings.

---
+++ web/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://emsifa.github.io',
+  base: '/api-data-wilayah-v2/',
+});
